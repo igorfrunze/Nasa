164238b91f1f223324353dca9d6e6ccbd1673a60
igor_frunze <igor_frunze@live.com>
2025-04-28 15:44:01 +0300

    fin

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,13 +7,17 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customers: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
 
 function getAllLaunches() {
   return Array.from(launches.values());
@@ -22,17 +26,26 @@ function getAllLaunches() {
 function addNewLaunch(launch) {
   latestFlightNumber++;
   launches.set(
-    httpGetAllPlanets,
+    latestFlightNumber,
     Object.assign(launch, {
       success: true,
       upcoming: true,
-      customers: ['Zero to Master', 'NASA'],
+      customers: ['ZTM', 'NASA'],
       flightNumber: latestFlightNumber,
     })
   );
 }
 
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
   addNewLaunch,
+  abortLaunchById,
 };
